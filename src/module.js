43055ff06e192abd763aad9a0e208cd71f0038bc
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2019-02-10 22:31:34 -0200

    Update module

--- src/module.js
+++ src/module.js
@@ -1,72 +1,46 @@
 const path = require("path");
-const chalk = require("chalk");
 const fs = require("fs-extra");
-const utils = require("./utils");
 
 module.exports = function() {
     if (this.options.dev) {
-        // Add the special render route for dev mode.
         this.extendRoutes((routes, resolve) => {
-            routes.push({
-                path: process.env.RENDER_PATH,
-                component: resolve(this.options.srcDir, "pages/index.vue"),
-            });
-        });
-    } else {
-        this.nuxt.hook("generate:done", async () => {
-            // Render the HTML template.
-            const { html } = await this.nuxt.renderer.renderRoute("/", {
-                url: "/",
-            });
-            fs.writeFileSync(
-                path.resolve(this.options.generate.dir, "index.html"),
-                html,
+            const index = routes.find(
+                // Try our best to find the root route.
+                // First, check if there's a route at /.
+                // Then, check i18n routes.
+                route =>
+                    route.path === "/" ||
+                    (route.name && route.name.match(/^index-\w+$/)),
             );
-
-            // Render the JSON manifest (useful to render a custom HTML template).
-            fs.copySync(
-                path.resolve(
-                    this.options.buildDir,
-                    "dist/server/client.manifest.json",
-                ),
-                path.resolve(this.options.generate.dir, "client.manifest.json"),
+            routes.push(
+                Object.assign({}, index, {
+                    name: "__laravel_nuxt__",
+                    path: process.env.RENDER_PATH,
+                }),
             );
-
-            // Move the compiled assets to the public directory.
-            if (!utils.isUrl(this.options.build.publicPath)) {
-                fs.moveSync(
-                    path.resolve(
-                        this.options.generate.dir,
-                        utils.normalizePublicPath(
-                            this.options.build.publicPath,
-                        ),
-                    ),
-                    path.resolve(
-                        "public",
-                        utils.normalizePublicPath(
-                            this.options.build.publicPath,
-                        ),
-                    ),
-                    {
-                        overwrite: true,
-                    },
-                );
-            } else {
-                console.log(
-                    `${chalk.green(
-                        `[laravel-nuxt]`,
-                    )} Looks like you are using a CDN to serve your assets [${
-                        this.options.build.publicPath
-                    }]`,
-                );
-                console.log(
-                    `${chalk.green(
-                        `[laravel-nuxt]`,
-                    )} You should now publish the contents of ${
-                        this.options.generate.dir
-                    } there`,
-                );
-            }
         });
+        return;
     }
+
+    const publicPath = path.resolve("public" + this.options.build.publicPath);
+
+    this.nuxt.hook("generate:done", async () => {
+        const { html } = await this.nuxt.renderer.renderRoute("/", {
+            url: "/",
+        });
+
+        fs.moveSync(
+            path.resolve(
+                this.options.generate.dir + this.options.build.publicPath,
+            ),
+            publicPath,
+            {
+                overwrite: true,
+            },
+        );
+
+        fs.writeFileSync(path.resolve(publicPath, "index.html"), html);
+
+        fs.removeSync(path.resolve(this.options.generate.dir));
+    });
 };
